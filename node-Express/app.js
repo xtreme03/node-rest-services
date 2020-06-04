752d2ec9f8a2128e30aca121eda67621c479b5f2
xtreme03 <pallabnag9@gmail.com>
2020-06-05 01:26:17 +0530

    Added Express layer with html content

--- node-Express/app.js
+++ node-Express/app.js
@@ -1,30 +1,24 @@
 const http=require('http');
+const path= require('path')
 
 const express=require('express');
 const bodyParser=require('body-parser');
 
+const adminRoutes=require('./routes/admin')
+const shopRoutes=require('./routes/shop')
+
 const app=express();
 
 app.use(bodyParser.urlencoded({extended:false}));
-
-app.use('/',(req,res,next)=>{
-    console.log('This always runs');
-    next();
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin',(res,req,next)=>{console.log("authentication layer"); next();},adminRoutes);
+app.use(shopRoutes);
+app.use((req,res,next)=>{
+    //res.status(404).send('<h1>Page not found</h1>')
+    res.status(404).sendFile(path.join(__dirname  ,'views','404.html'))
 })
 
-app.use('/add-product',(req,res,next)=>{
-    console.log('In another middleware');
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
 
-})
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');   
-})
-app.use('/',(req,res,next)=>{
-    console.log('In the last middleware');
-    res.send('<h1>Hello from Pallab</h1>');  
-})
 
 const server=http.createServer(app);
 

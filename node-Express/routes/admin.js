8eb8840869e77e5aec7735e8b704f1bc5ed5fdcb
xtreme03 <pallabnag9@gmail.com>
2020-06-05 01:28:27 +0530

    html added

---
+++ node-Express/routes/admin.js
@@ -0,0 +1,16 @@
+const path=require('path')
+
+const express=require('express')
+
+const router=express.Router();
+router.get('/add-product',(req,res,next)=>{
+    console.log('In another middleware');
+    //res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'))
+})
+router.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');   
+})
+
+module.exports=router;

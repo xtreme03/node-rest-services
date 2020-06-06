d736b62c6d7b601500fed125b13172d1dcd2dfd9
xtreme03 <pallabnag9@gmail.com>
2020-06-07 01:38:15 +0530

    model with local variable

--- node-Express/controllers/product.js
+++ node-Express/controllers/product.js
@@ -1,3 +1,5 @@
+const Product=require('../model/product')
+
 exports.getAddProduct=(req,res,next)=>{
     console.log('In another middleware');
     //res.send('<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
@@ -9,16 +11,17 @@ exports.getAddProduct=(req,res,next)=>{
         productCSS:true,
         activeAddProduct:true
     })}
-const products = [];
 exports.addProduct=(req,res,next)=>{
     console.log(req.body);
-    products.push({ title: req.body.title });
+    const product=new Product(req.body.title);
+    product.save()
     res.redirect('/');   
 }
 
 exports.getProduct=(req,res,next)=>{
     //console.log('In the last middleware');
     //res.send('<h1>Hello from Pallab</h1>'); 
+    const products =Product.fetchAll()
     console.log('shop.js', products);
     //res.sendFile(path.join(__dirname,'../','views','shop.html'))
     res.render('shop', {
